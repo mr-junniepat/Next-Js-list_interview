a861bb94187f961fd0294452a52045f17ef59008
mr-junniepat <patrickoigwe@gmail.com>
2022-10-19 14:32:07 +0400

    Tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -119,7 +119,7 @@ const Home = () => {
 
 
       <div className="bg-white">
-        <div className={'mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'}>
+        <div className={'mx-auto max-w-2xl py-6 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8'}>
         
         {/* {banner.position === "top" && <>banner</>} */}
 
